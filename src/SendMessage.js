69ce03a50bc291df6c21e6ce1126f41bb514eff2
Luke-Falanga <falanglv@mail.uc.edu>
2026-06-11 23:44:03 +0000

    Created send_message.html and SendMessage.js files to create SendMessage UI with a Message Box

--- src/SendMessage.js
+++ src/SendMessage.js
@@ -1,11 +1,37 @@
-// This file will cover javascript functionality for Use-Case-01: Send Message
+// UI References
+var messageList = document.getElementById("messages");
+
+var sendBtnElm = document.getElementById('send-button');
+if(!sendBtnElm) {
+    console.log("Error in getting 'send-button' button");
+}
+// AC-01.1: Send button click triggers sendMessage()
+sendBtnElm.addEventListener('click', sendMessage);
+
+var chatMessageInput = document.getElementById('chat-message');
+if(!chatMessageInput) {
+    console.log("Error in getting 'chat-message' input");
+}
+
+chatMessageInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') sendMessage();
+});
+
+// ==================================================
+// Use-Case-01: Send Message
+// ==================================================
 
 function sendMessage() {
-    var message = chatMessageInput.value.trim();
-    if (!message) return;   // AC-02.2: Empty messages are ignored
+    var input = chatMessageInput.value.trim();
+    if (!input) return;   // AC-02.2: Empty messages are ignored
 
-    //for current UI testing, the message is sent to the console. Later it will be sent to a message box later with web sockets.
-    console.log(`Debug>Chat message: ${message}`); // AC-01.4: The message displays the sender's username alongside the message text
+    const msg = document.createElement("div");
+    msg.className = "message";
+    msg.textContent = input;
+
+    messageList.appendChild(msg);
+    messageList.scrollTop = messageList.scrollHeight;
+
     chatMessageInput.value = ''; // AC-01.3: clear input after sending
     chatMessageInput.focus();
 }
--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 // UI References
 import sendMessage from sendMessage.js;
 
+var messageList = document.getElementById("messages");
+
 var sendBtnElm = document.getElementById('send-button');
 if(!sendBtnElm) {
     console.log("Error in getting 'send-button' button");
